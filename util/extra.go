1dd9b2617435526544605ba7d8f99b3a330c66af
Adam Leskis <leskis@gmail.com>
2023-06-24 11:37:40 +0100

    fix double comma

--- util/extra.go
+++ util/extra.go
@@ -154,7 +154,7 @@ type FakePurchase struct {
 	PurchaseItem     string
 	// literally just putting this int stuff in so we can have an exercise for an array of ints
 	PurchaseCode  int     `faker:"oneof: 4, 9, 18, 55, 102, 188, 225, 801, 3997"`
-	PurchasePrice float64 `faker:"oneof: 1.99, 3.50, 10.81, 12.18, 19.99,, 22.00, 25.50, 31.67, 38.50, 40.99, 45.00, 67.20, 69.99, 76.89, 85.15, 90.12"`
+	PurchasePrice float64 `faker:"oneof: 1.99, 3.50, 10.81, 12.18, 19.99, 22.00, 25.50, 31.67, 38.50, 40.99, 45.00, 67.20, 69.99, 76.89, 85.15, 90.12"`
 }
 
 // previously using faker for this, but it sometimes output duplicate names
